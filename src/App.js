285bd4893c965b0c33991119a988dd2942b2b2c8
ANNA MASUMYAN <a.masumyan1985@gmail.com>
2023-02-27 17:54:32 +0400

    creating Parallax effect in React.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Parallax } from "react-parallax";
+import Par1 from "../src/images/parallax1.jpg";
+import Par2 from "../src/images/parallax2.jpg";
+import Par3 from "../src/images/parallax3.webp";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Parallax strength={600} bgImage={Par1}>
+        <div className="content">
+          <div className="text-content">Normal Parallax</div>
+        </div>
+      </Parallax>
+      <Parallax strength={300} blur={{min: -15, max: 15}} bgImage={Par3}>
+        <div className="content">
+          <div className="text-content">Blur Parallax</div>
+        </div>
+      </Parallax>
+      <Parallax strength={-600} bgImage={Par2}>
+        <div className="content">
+          <div className="text-content">Reverse Parallax</div>
+        </div>
+      </Parallax>
+      <div className="content"></div>
     </div>
   );
 }
